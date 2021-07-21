02b7f5bcff8fd4934c4c30d1eb120c7b386d25f2
Thomas Clausen <topperhdoriginal@gmail.com>
2021-07-20 18:12:31 -0700

    dist deleted

--- src/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bot_1 = require("./bot");
-let client = new bot_1.Client();
-client.Login();
-//# sourceMappingURL=app.js.map
